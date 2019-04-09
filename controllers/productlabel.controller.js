b04526324d90557660fe4f5c97ce18d90a99eb0a
Ashish <ashish.a@datafoundry.ai>
2019-04-09 14:44:27 +0530

    Adds missing dependency

--- controllers/productlabel.controller.js
+++ controllers/productlabel.controller.js
@@ -9,6 +9,8 @@ var rp = require('request-promise');
 var fs = require('fs');
 var uuid = require('uuid-v4');
 const appConfig = require('../config/appConfig');
+var _ = require('lodash');
+require('mongoose').set('debug', true);
 
 exports.newProject = function (req, res) {
     var productLabel = new ProductLabel();
@@ -76,98 +78,68 @@ exports.compare = function (req, res) {
         .then(function (_project) {
             project = _project;
             var coreDoc;
-            // if (project && project.documents != null && project.documents.length > 0) {
-            var payload = {
-                "label_filepath": "",
-                "ha_filepath": [],
-                "checklist_filepath": [],
-                "previousLabel_filepath": [],
-                "fontFormat_filepath": [],
-                "reference_filepath": [],
-            };
-            var basePath = path.resolve('./');
+            if (project && project.documents != null && project.documents.length > 0) {
+                var payload = {
+                    "label_filepath": "",
+                    "ha_filepath": [],
+                    "checklist_filepath": [],
+                    "previousLabel_filepath": [],
+                    "fontFormat_filepath": [],
+                    "reference_filepath": [],
+                };
+                var basePath = path.resolve('./');
 
-            project.documents.forEach(element => {
-                var filePath = path.resolve(basePath, element.location, element.documentName);
-                switch (element.fileType) {
-                    case 'Label':
-                        coreDoc = element;
-                        conflictDoc = _.cloneDeep(coreDoc);
-                        // var id = uuid();
-                        // conflictDoc.documentId = id;
-                        // conflictDoc.fileType = 'CONFLICT';
-                        // conflictDoc.destination = fileVirtualPath + "/" + id + '/' + element.documentName;
-                        // conflictDoc.location = fileUploadPath + '/' + id;
-                        // payload.label_filepath = path.resolve('./', conflictDoc.location, conflictDoc.documentName);
-                        break;
-                    case 'Reference':
-                        payload.reference_filepath.push(filePath);
-                        break;
-                    case 'Previous Label':
-                        payload.reference_filepath.push(filePath);
-                        break;
-                    case 'HA Guidelines':
-                        payload.ha_filepath.push(filePath);
-                        break;
-                    case 'Pfizer Checklist':
-                        payload.checklist_filepath.push(filePath);
-                        break;
-                    case 'Font Format Spec':
-                        payload.fontFormat_filepath.push(filePath);
-                        break;
+                project.documents.forEach(element => {
+                    var filePath = path.resolve(basePath, element.location, element.documentName);
+                    switch (element.fileType) {
+                        case 'Label':
+                            coreDoc = element;
+                            conflictDoc = _.cloneDeep(coreDoc);
+                            // var id = uuid();
+                            // conflictDoc.documentId = id;
+                            // conflictDoc.fileType = 'CONFLICT';
+                            // conflictDoc.destination = fileVirtualPath + "/" + id + '/' + element.documentName;
+                            // conflictDoc.location = fileUploadPath + '/' + id;
+                            payload.label_filepath = path.resolve('./', conflictDoc.location, conflictDoc.documentName);
+                            break;
+                        case 'Reference':
+                            payload.reference_filepath.push(filePath);
+                            break;
+                        case 'Previous Label':
+                            payload.reference_filepath.push(filePath);
+                            break;
+                        case 'HA Guidelines':
+                            payload.ha_filepath.push(filePath);
+                            break;
+                        case 'Pfizer Checklist':
+                            payload.checklist_filepath.push(filePath);
+                            break;
+                        case 'Font Format Spec':
+                            payload.fontFormat_filepath.push(filePath);
+                            break;
+                    }
+                });
+                const options = {
+                    uri: 'http://34.204.2.145:3001/',
+                    method: 'POST',
+                    json: true,
+                    body: payload,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
                 }
-            });
 
-            payload = {
-                "lpd_filepath": "C:\\Users\\Reeth\\Desktop\\lpd.docx",
-                "ha_filepath": ["C:\\Users\\Reeth\\Desktop\\TemplateSPC-PIL-Labeling.pdf"]
-            };
-            // fs.copyFileSync(
-            //     path.resolve('./', coreDoc.location, coreDoc.documentName),
-            //     path.resolve('./', conflictDoc.location, conflictDoc.documentName),
-            // )
-            // fileUploadPath = appConfig["FS_PATH"];
-
-            const options = {
-                uri: 'http://34.204.2.145:3001/',
-                method: 'POST',
-                json: true,
-                body: payload,
-                headers: {
-                    'Content-Type': 'application/json',
-                }
+                return rp(options);
+            } else {
+                throw new Error();
             }
-
-            return rp(options);
-            // } else {
-            //     throw new Error();
-            // }
         }).then(function (result) {
             project.conflicts = result.conflicts;
-            project.conflicts.type = result.conflicts.conflict_type;
+            project.conflicts.types = result.conflicts.conflict_type;
             project.conflicts.comments = result.comments;
-            // if (result.filePath) {
-
-            // var documentId = uuid();
-            // var conflictDoc = {
-            //     documentName: path.basename(result.filePath),
-            //     destination = fileVirtualPath + "/" + documentId + "/" + this.documentName,
-            //     documentid: documentId,
-            //     // documentSchema.version = "0.1";
-
-            // }
-            // var documentSchema = new DocumentSchema();
-            // documentSchema.projectId = req.query.projectId;
-            // documentSchema.fileType = req.query.fileType;
-            // documentSchema.location = fileUploadPath;
-            // documentSchema.uploadedBy = req.query.uploadedBy;
-            // documentSchema.uploadedDate = new Date();
-            // }
-
             return project.save();
         }).then(function (projectObj) {
-            console.log("TCL: exports.compare -> projectObj", projectObj)
-
+            return res.send(responseGenerator(1, "Compared document", projectObj));
         }).catch(function (err) {
             console.log(err);
             return res.status(400).send({ success: false, err: err.message });
@@ -177,25 +149,15 @@ exports.compare = function (req, res) {
 
 
 exports.updateProject = function (req, res) {
-    try {
-        var productLabel = new ProductLabel();
-        productLabel._id = req.body._id;
-        productLabel.productName = req.body.productName;
-        productLabel.ownerName = req.body.ownerName;
-        productLabel.modifiedDate = req.body.modifiedDate;
-        productLabel.country = req.body.country;
-        productLabel.exceptedEndDate = req.body.exceptedEndDate;
-        productLabel.documents = req.body.documents;
-        productLabel.findByIdAndUpdate(req.body._id, { $set: productLabel }, {
-            new: false
-        }, function (err, request) {
-            if (err) {
-                res.json(responseGenerator(-1, "Unable to update the Project details", err));
-            } else {
-                res.json(responseGenerator(0, "Successfully updated the Project details", productLabel, 0));
-            }
-        });
-    } catch (e) {
-        console.log(e);
-    }
+
+    var productLabel = new ProductLabel(req.body);
+    return ProductLabel.findByIdAndUpdate(req.body._id,
+        { $set: productLabel },
+        { new: false }
+    ).then(function () {
+        res.json(responseGenerator(0, "Successfully updated the Project details", productLabel, 0));
+    }).catch(function (err) {
+        res.json(responseGenerator(-1, "Unable to update the Project details", err));
+    });
+
 };
