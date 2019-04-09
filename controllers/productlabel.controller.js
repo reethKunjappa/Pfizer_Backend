7d0b93d0c6765628492b1078bb3814222024594b
Ashish <>
2019-04-09 13:18:18 +0530

    Updates Conflicts API

--- controllers/productlabel.controller.js
+++ controllers/productlabel.controller.js
@@ -4,16 +4,22 @@ const { ProductLabel, DocumentSchema } = require('../models/model');
 const { responseGenerator } = require('../utility/commonUtils');
 var { mkdir } = require('../utility/commonUtils');
 var http = require('http');
+var path = require('path');
+var rp = require('request-promise');
+var fs = require('fs');
+var uuid = require('uuid-v4');
+const appConfig = require('../config/appConfig');
 
 exports.newProject = function (req, res) {
     var productLabel = new ProductLabel();
-    var conflicts =  {
+    var conflicts = {
         number: 0,
         types: {
             fontConflicts: 0,
             contentConflicts: 0,
             orderConflicts: 0,
-        }};
+        }
+    };
     productLabel.projectName = req.body.projectName;
     productLabel.country = req.body.country;
     productLabel.createdBy = req.body.createdBy;
@@ -33,7 +39,7 @@ exports.newProject = function (req, res) {
 
 exports.getProjects = function (req, res) {
     try {
-        ProductLabel.find({}).sort({modifiedDate: 'desc'}).exec(function (err, projects) {
+        ProductLabel.find({}).sort({ modifiedDate: 'desc' }).exec(function (err, projects) {
             if (err)
                 res.json(responseGenerator(-1, "Unable to retrieve Projects list", err));
             else
@@ -59,88 +65,114 @@ exports.viewProject = function (req, res) {
 };
 
 exports.compare = function (req, res) {
-    try {
-        var compareRequest = [];
-        var LPD_Document = null;
-        ProductLabel.findOne({ _id: req.body._id }).populate('documents').exec(function (err, project) {
-            if (err)
-                res.json(responseGenerator(-1, "Unable to fetch the Project details", err));
-            else {
-                if (project && project.documents != null && project.documents.length > 0) {
-                    project.documents.forEach(element => {
-                        // console.log(element);
-                        var fileData = { path: "", file_name: "", file_type: "", country_name: "" };
-                        fileData.path = "/home/ubuntu/pfizer/pfizer-back-nodejs/fs/" + element.documentid;
-                        fileData.file_name = element.documentName;
-                        fileData.file_type = element.fileType;
+    var project = {};
+    var conflictDoc = {
+        type: 'CONFLICT'
+    };
+    var fileUploadPath = appConfig["FS_PATH"];
+    var fileVirtualPath = appConfig["DOCUMENT_VIEW_PATH"];
 
-                        // Getting the Document ID for Conflict Update
-                        if (fileData.file_type == 'LPD') {
-                            LPD_Document = element;
-                        }
-                        compareRequest.push(fileData);
-                    });
-                    // console.log(compareRequest);
+    return ProductLabel.findOne({ _id: req.body._id }).populate('documents')
+        .then(function (_project) {
+            project = _project;
+            var coreDoc;
+            // if (project && project.documents != null && project.documents.length > 0) {
+            var payload = {
+                "label_filepath": "",
+                "ha_filepath": [],
+                "checklist_filepath": [],
+                "previousLabel_filepath": [],
+                "fontFormat_filepath": [],
+                "reference_filepath": [],
+            };
+            var basePath = path.resolve('./');
 
-                    const data = JSON.stringify(compareRequest);
-                    const options = {
-                        hostname: '13.233.129.104', // '127.0.0.1',
-                        port: 5013,
-                        path: '/conflict_retrieva                                               l',
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        }
-                    }
-                    // console.log(data);
-                    const reqq = http.request(options, (resp) => {
-                        var buffers = []
-                        if (resp.statusCode == 200) {
-                            resp.on('data', function (body) {
-                                buffers.push(body)
-                            }).on('end', function () {
-                                var conflicts = JSON.parse(Buffer.concat(buffers).toString());
-                                // console.log(LPD_Document);
-                                console.log(conflicts);
-
-                                if (conflicts) {
-                                    var documentSchema = new DocumentSchema();
-                                    documentSchema._id = LPD_Document._id;
-                                    documentSchema.conflicts = conflicts;
-                                    // console.log(LPD_Document);
-                                    DocumentSchema.findByIdAndUpdate(LPD_Document._id, { $set: documentSchema }, {
-                                        new: false
-                                    }, function (err, request) {
-                                        if (err) {
-                                            res.json(responseGenerator(-1, "Unable to upload the conflict data", ""));
-                                        } else {
-                                            res.json(responseGenerator(0, "Successfully updated conflict data", conflicts));
-                                        }
-                                    });
-                                    // res.json(responseGenerator(0, "Successfully retrieved Conflict details", conflicts, 0));
-                                }
-
-                            });
-                        }
-
-                    })
-
-                    reqq.on('error', (error) => {
-                        res.json(responseGenerator(-1, "Unable to fetch the Conflict details", err));
-                    })
-
-                    reqq.write(data)
-                    reqq.end()
+            project.documents.forEach(element => {
+                var filePath = path.resolve(basePath, element.location, element.documentName);
+                switch (element.fileType) {
+                    case 'Label':
+                        coreDoc = element;
+                        conflictDoc = _.cloneDeep(coreDoc);
+                        // var id = uuid();
+                        // conflictDoc.documentId = id;
+                        // conflictDoc.fileType = 'CONFLICT';
+                        // conflictDoc.destination = fileVirtualPath + "/" + id + '/' + element.documentName;
+                        // conflictDoc.location = fileUploadPath + '/' + id;
+                        // payload.label_filepath = path.resolve('./', conflictDoc.location, conflictDoc.documentName);
+                        break;
+                    case 'Reference':
+                        payload.reference_filepath.push(filePath);
+                        break;
+                    case 'Previous Label':
+                        payload.reference_filepath.push(filePath);
+                        break;
+                    case 'HA Guidelines':
+                        payload.ha_filepath.push(filePath);
+                        break;
+                    case 'Pfizer Checklist':
+                        payload.checklist_filepath.push(filePath);
+                        break;
+                    case 'Font Format Spec':
+                        payload.fontFormat_filepath.push(filePath);
+                        break;
                 }
-                else{
-                    res.json(responseGenerator(-1, "Unable to fetch the Project details", project));
+            });
+
+            payload = {
+                "lpd_filepath": "C:\\Users\\Reeth\\Desktop\\lpd.docx",
+                "ha_filepath": ["C:\\Users\\Reeth\\Desktop\\TemplateSPC-PIL-Labeling.pdf"]
+            };
+            // fs.copyFileSync(
+            //     path.resolve('./', coreDoc.location, coreDoc.documentName),
+            //     path.resolve('./', conflictDoc.location, conflictDoc.documentName),
+            // )
+            // fileUploadPath = appConfig["FS_PATH"];
+
+            const options = {
+                uri: 'http://34.204.2.145:3001/',
+                method: 'POST',
+                json: true,
+                body: payload,
+                headers: {
+                    'Content-Type': 'application/json',
                 }
             }
-        });
 
-    } catch (e) {
-        console.log(e);
-    }
+            return rp(options);
+            // } else {
+            //     throw new Error();
+            // }
+        }).then(function (result) {
+            project.conflicts = result.conflicts;
+            project.conflicts.type = result.conflicts.conflict_type;
+            project.conflicts.comments = result.comments;
+            // if (result.filePath) {
+
+            // var documentId = uuid();
+            // var conflictDoc = {
+            //     documentName: path.basename(result.filePath),
+            //     destination = fileVirtualPath + "/" + documentId + "/" + this.documentName,
+            //     documentid: documentId,
+            //     // documentSchema.version = "0.1";
+
+            // }
+            // var documentSchema = new DocumentSchema();
+            // documentSchema.projectId = req.query.projectId;
+            // documentSchema.fileType = req.query.fileType;
+            // documentSchema.location = fileUploadPath;
+            // documentSchema.uploadedBy = req.query.uploadedBy;
+            // documentSchema.uploadedDate = new Date();
+            // }
+
+            return project.save();
+        }).then(function (projectObj) {
+            console.log("TCL: exports.compare -> projectObj", projectObj)
+
+        }).catch(function (err) {
+            console.log(err);
+            return res.status(400).send({ success: false, err: err.message });
+        })
+
 };
 
 
