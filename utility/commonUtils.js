b93dd3ad202906ff01f48af10b49b7614855c68f
Nagesh-jha <nagesh.jha@datafoundry.ai>
2019-08-30 12:12:53 +0530

    dwelete folder fixed

--- utility/commonUtils.js
+++ utility/commonUtils.js
@@ -1,9 +1,9 @@
-var mkdirp = require('mkdirp');
-const { getModel } = require('../models/model');
-var rimraf = require('rimraf');
-require('dotenv').config();
-const { PYTHON_URL_W2P } = require('../config/appConfig');
-const v = require('node-input-validator');
+var mkdirp = require("mkdirp");
+const { getModel } = require("../models/model");
+var rimraf = require("rimraf");
+require("dotenv").config();
+const { PYTHON_URL_W2P } = require("../config/appConfig");
+const v = require("node-input-validator");
 var path = require("path");
 //var pythonConvertAPI = "http://localhost:3009/";
 
@@ -22,41 +22,41 @@ var path = require("path");
   });
 }; */
 
-exports.convertDocToPdf = function (fpath,_id) {
-    console.log("******Node calling python convertToPDF mode*******");
-    console.log("File Path: ", fpath)
-    console.log("_id: ", _id);
-    let pdfPath = fpath.replace(path.extname(fpath), ".pdf"); 
-    deleteFolder(path.resolve(pdfPath)); //remove exsting Pdf file 
-    var rp = require('request-promise');
-    var options = {
-        method: 'POST',
-        uri: PYTHON_URL_W2P,
-        body: {
-            file_path: fpath,
-            project_id: _id
-        },
-        json: true // Automatically stringifies the body to JSON
-    };
+exports.convertDocToPdf = function(fpath, _id) {
+  console.log("******Node calling python convertToPDF mode*******");
+  console.log("File Path: ", fpath);
+  console.log("_id: ", _id);
+  let pdfPath = fpath.replace(path.extname(fpath), ".pdf");
+  deleteFolder(path.resolve(pdfPath)); //remove exsting Pdf file
+  var rp = require("request-promise");
+  var options = {
+    method: "POST",
+    uri: PYTHON_URL_W2P,
+    body: {
+      file_path: fpath,
+      project_id: _id
+    },
+    json: true // Automatically stringifies the body to JSON
+  };
 
-    return rp(options);
-}
-
-exports.getCount = function (model, condition) {
-    return new Promise(function (resolve, reject) {
-        model.countDocuments(condition, function (err, count) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(count);
-            }
-        });
-    });
+  return rp(options);
 };
 
-exports.deleteFolder = function (dirPath) {
-    return rimraf.sync(dirPath);
-}
+exports.getCount = function(model, condition) {
+  return new Promise(function(resolve, reject) {
+    model.countDocuments(condition, function(err, count) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(count);
+      }
+    });
+  });
+};
+
+var deleteFolder = (exports.deleteFolder = function(dirPath) {
+  return rimraf.sync(dirPath);
+});
 /* exports.getSequenceNumber = function (preceed, model, condition) {
     return new Promise(function (resolve, reject) {
         let year = new Date().getFullYear();
@@ -101,13 +101,17 @@ exports.deleteFolder = function (dirPath) {
     })
 }; */
 
-exports.responseGenerator = function (valid, msg, output, count) {
-    return { status: { code: valid, message: msg }, result: output, totalRecords: count };
+exports.responseGenerator = function(valid, msg, output, count) {
+  return {
+    status: { code: valid, message: msg },
+    result: output,
+    totalRecords: count
+  };
 };
 
-exports.mkdir = function (path) {
-    mkdirp(path, function (err) { });
-}
+exports.mkdir = function(path) {
+  mkdirp(path, function(err) {});
+};
 
 /* function padDigits(number, digits) {
     return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
@@ -122,44 +126,41 @@ exports.auditReport = (user,description,project,actionType) =>{
     }
 }; */
 
+exports.inputValidator = async (body, tempObj) => {
+  let obj = { status: 200, message: "", isValid: true };
+  let validator = new v(body, tempObj);
 
-exports.inputValidator =  async (body,tempObj) =>{
-    let obj ={status : 200,
-        message : "",
-        isValid : true}
-    let validator = new v( body, tempObj);
+  let matched = await validator.check();
 
-    let matched = await validator.check();
- 
-    if (!matched) {
-        let obj ={status : 422,
-         message : validator.errors,
-         isValid : false}
-        return obj;
-    }
-
+  if (!matched) {
+    let obj = { status: 422, message: validator.errors, isValid: false };
     return obj;
- 
-}
+  }
 
-const bunyan = require('bunyan');
+  return obj;
+};
+
+const bunyan = require("bunyan");
 exports.log = bunyan.createLogger({
-    name: 'pfizer',
-    serializers: {err: bunyan.stdSerializers.err,req: bunyan.stdSerializers.req},
-    streams: [
-      {
-        level: 'info',
-        type: 'rotating-file',
-        path: './logs/info.log',            // log INFO and above to a file
-        period: '1d',   // daily rotation
-        count: 3        // keep 3 back copies
-      },
-      {
-        level: 'error',
-        type: 'rotating-file',
-        path: './logs/error.log',  // log ERROR and above to a file
-        period: '1d',   // daily rotation
-        count: 3        // keep 3 back copies
-      }
-    ]
-  });
+  name: "pfizer",
+  serializers: {
+    err: bunyan.stdSerializers.err,
+    req: bunyan.stdSerializers.req
+  },
+  streams: [
+    {
+      level: "info",
+      type: "rotating-file",
+      path: "./logs/info.log", // log INFO and above to a file
+      period: "1d", // daily rotation
+      count: 3 // keep 3 back copies
+    },
+    {
+      level: "error",
+      type: "rotating-file",
+      path: "./logs/error.log", // log ERROR and above to a file
+      period: "1d", // daily rotation
+      count: 3 // keep 3 back copies
+    }
+  ]
+});
